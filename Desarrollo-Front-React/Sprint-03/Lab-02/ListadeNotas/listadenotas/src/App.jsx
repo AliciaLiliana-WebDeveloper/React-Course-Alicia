01b76ce08f118ae2d05ffe6dabeef537e119a205
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2025-11-23 22:56:42 +0100

    Video 2: Mouse Position Component

--- Desarrollo-Front-React/Sprint-03/Lab-02/ListadeNotas/listadenotas/src/App.jsx
+++ Desarrollo-Front-React/Sprint-03/Lab-02/ListadeNotas/listadenotas/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ListaDeNotas from "./ListaDeNotas";
+import MousePositionComponent from "./MousePositionComponent";
 
 function App() {
   return (
     <div>
-      <ListaDeNotas />
+      <MousePositionComponent />
     </div>
   );
 }
