62a630a5fb885bd8bc43ddc498563ba94e6213d2
AliciaLilianaEEP <145449008+AliciaLilianaEEP@users.noreply.github.com>
2025-11-23 22:45:17 +0100

    Video 1: Lista de Notas

---
+++ Desarrollo-Front-React/Sprint-03/Lab-02/ListadeNotas/listadenotas/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ListaDeNotas from "./ListaDeNotas";
+
+function App() {
+  return (
+    <div>
+      <ListaDeNotas />
+    </div>
+  );
+}
+
+export default App;
